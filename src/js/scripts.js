d5acb45c910947d8d020759de0c75f5bea4533bd
Brian Maier Jr <brimaidesigns@gmail.com>
2021-01-11 23:48:35 -0500

    + work examples and copy

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,33 +1,16 @@
-// filter handling for a /dir/ OR /indexordefault.page
-function filterPath(string) {
-  return string
-    .replace(/^\//, '')
-    .replace(/(index|default).[a-zA-Z]{3,4}$/, '')
-    .replace(/\/$/, '');
-} 
-
-var locationPath = filterPath(location.pathname); 
-$('a[href*="#"].scroll').each(function () {
-  var thisPath = filterPath(this.pathname) || locationPath;
-  var hash = this.hash;
-  if ($("#" + hash.replace(/#/, '')).length) {
-    if (locationPath == thisPath && (location.hostname == this.hostname || !this.hostname) && this.hash.replace(/#/, '')) {
-      var $target = $(hash), target = this.hash;
-      if (target) {
-        $(this).click(function (event) {
-          event.preventDefault();
-          $('html, body').animate({scrollTop: $target.offset().top}, 1000, function () {
-            location.hash = target; 
-            $target.focus();
-            if ($target.is(":focus")){ //checking if the target was focused
-              return false;
-            }else{
-              $target.attr('tabindex','-1'); //Adding tabindex for elements not focusable
-              $target.focus(); //Setting focus
-            };
-          });       
-        });
-      }
-    }
-  }
-});
+const links = document.querySelectorAll("a.scroll");
+ 
+for (const link of links) {
+  link.addEventListener("click", clickHandler);
+}
+ 
+function clickHandler(e) {
+  e.preventDefault();
+  const href = this.getAttribute("href");
+  const offsetTop = document.querySelector(href).offsetTop;
+ 
+  scroll({
+    top: offsetTop,
+    behavior: "smooth"
+  });
+}
