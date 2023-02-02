5ef0b7a0746e7a18d7a340a41c40432f9a632bf2
Brian Maier <brimaidesigns@gmail.com>
2023-02-01 22:54:17 -0500

    fix cms setup

--- .eleventy.js
+++ .eleventy.js
@@ -1,24 +1,27 @@
-module.exports = function(eleventyConfig) {
-  eleventyConfig.addShortcode('excerpt', article => extractExcerpt(article));
-  eleventyConfig.addWatchTarget('_site/assets/css/*.css');
-  eleventyConfig.addPassthroughCopy({'src/img': 'assets/img'});
-  eleventyConfig.addPassthroughCopy({'src/fonts': 'assets/fonts'});
-  eleventyConfig.addPassthroughCopy({'src/js': 'assets/js'});
-  eleventyConfig.addPassthroughCopy({'src/favicon': './'});
-  eleventyConfig.addPassthroughCopy('src/_redirects');
-  eleventyConfig.setBrowserSyncConfig({
-    files: ['_site/assets/css/*.css']
-  });
+module.exports = function (eleventyConfig) {
+	eleventyConfig.addShortcode("excerpt", (article) =>
+		extractExcerpt(article)
+	);
+	eleventyConfig.addWatchTarget("_site/assets/css/*.css");
+	eleventyConfig.addPassthroughCopy({ "src/img": "assets/img" });
+	eleventyConfig.addPassthroughCopy({ "src/fonts": "assets/fonts" });
+	eleventyConfig.addPassthroughCopy({ "src/js": "assets/js" });
+	eleventyConfig.addPassthroughCopy({ "src/favicon": "./" });
+	eleventyConfig.addPassthroughCopy("src/_redirects");
+	eleventyConfig.addPassthroughCopy({
+		"./src/admin/config.yml": "./admin/config.yml",
+	});
+	eleventyConfig.setBrowserSyncConfig({
+		files: ["_site/assets/css/*.css"],
+	});
 
-  return {
-    dir: {
-      // ⚠️ These values are both relative to your input directory.
-      input: "src",
-      includes: "_includes",
-      layouts: "_layouts"
-    },
-    // pathPrefix: "/portfolio2021/"  
-  };
+	return {
+		dir: {
+			// ⚠️ These values are both relative to your input directory.
+			input: "src",
+			includes: "_includes",
+			layouts: "_layouts",
+		},
+		// pathPrefix: "/portfolio2021/"
+	};
 };
-
-
